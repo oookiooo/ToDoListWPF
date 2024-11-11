fa48ad5110887f281005a3e89f8804d353930418
Arkadiusz Kiszka <00oookiocz@gmail.com>
2024-11-11 20:59:45 +0100

    [ToDoListWPF-001] Create migrations and ApiService WPF

--- ToDoListWPF/ApiServices/ApiService.cs
+++ ToDoListWPF/ApiServices/ApiService.cs
@@ -11,5 +11,42 @@ namespace ToDoListWPF.ApiServices
 {
     public class ApiService
     {
+        private readonly HttpClient _httpClient;
+        private HttpClient httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:7020/") };
+        }
+
+        public ApiService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<TaskItem>> GetTaskItems()
+        {
+            return await _httpClient.GetFromJsonAsync<List<TaskItem>>("api/TaskItems");
+        }
+
+        public async Task<TaskItem> GetTaskItem(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<TaskItem>($"api/TaskItems/{id}");
+        }
+
+        public async Task AddTaskItem(TaskItem taskItem)
+        {
+            await _httpClient.PostAsJsonAsync("api/TaskItems", taskItem);
+        }
+
+        public async Task UpdateTaskItem(TaskItem taskItem)
+        {
+            await _httpClient.PutAsJsonAsync($"api/TaskItems/{taskItem.Id}", taskItem);
+        }
+
+        public async Task DeleteTaskItem(int id)
+        {
+            await _httpClient.DeleteAsync($"api/TaskItems/{id}");
+        }
     }
 }
